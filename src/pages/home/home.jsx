b59599cb648c62dd567a73410679df5d7a129322
ishola360 <isholah360.com>
2024-09-11 17:27:40 +0100

    secvit

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar/navbar'
+
+function Home() {
+  return (
+    <div>
+      Home
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
